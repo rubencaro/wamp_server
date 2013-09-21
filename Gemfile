053c7d649dcaf68ca3784d567c20d6cfc8c6122b
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-09-21 23:16:51 +0200

    Get it working

--- Gemfile
+++ Gemfile
@@ -2,12 +2,15 @@ source 'https://rubygems.org'
 gem 'websocket-eventmachine-server'
 gem 'em-synchrony'
 gem 'mongo'
+gem 'bson_ext'
 gem 'fiber_connection_pool'
 gem 'mongo-em-patch', :github => 'rubencaro/mongo-em-patch'
 
 group :development, :test do
   gem 'pry'
   gem 'pry-doc'
+  gem 'pry-remote-em'
   gem 'minitest'
   gem 'rake'
+  gem 'websocket-eventmachine-client'
 end
--- main.rb
+++ main.rb
@@ -5,26 +5,46 @@ require 'em-synchrony'
 require 'mongo-em-patch'
 require 'fiber'
 
+module AppLogic
+  def self.do_things
+    res = MyServer.pool.collection('bogus').find( :$where => "sleep(2000)" ).count
+    "Done #{Thread.current.to_s}, #{Fiber.current.to_s} res:#{res.inspect}"
+  end
+end
+
+class MyServer < WebSocket::EventMachine::Server
+  def self.pool=(value); @@pool = value; end
+  def self.pool; @@pool; end
+
+  def self.stop
+    puts "Terminating WebSocket Server"
+    EventMachine.stop
+  end
+end
+
 EM.synchrony do
+  host = '0.0.0.0'
+  port = '3000'
 
-  @pool = FiberConnectionPool.new(:size => 5) do
-             Mongo::Connection.new.db('bogusdb')
-           end
+  trap("INT") { MyServer.stop }
 
-  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 3000) do |ws|
+  MyServer.pool = FiberConnectionPool.new(:size => 5) do
+                    Mongo::Connection.new.db('bogusdb')
+                  end
+
+  puts "Listening on #{host}:#{port}..."
+  MyServer.start(:host => host, :port => port) do |ws|
 
     ws.onopen do
       puts "Client connected"
     end
 
     ws.onmessage do |msg, type|
+      t1 = Time.now
       puts "Received message: #{msg}"
-      print '.'
       Fiber.new do
-        res = @pool.collection('bogus').find( :$where => "sleep(2000)" ).count
-        msg = "Done #{Thread.current.to_s}, #{Fiber.current.to_s} res:#{res.inspect}"
-        puts msg
-        ws.send msg, :type => type
+        res = AppLogic.do_things
+        ws.send res.to_s + " Took #{Time.now - t1} secs" , :type => type
       end.resume
     end
 
