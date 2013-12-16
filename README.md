1f001fd9dc740b9ff0d58dd76861db5232f02fb6
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-12-16 20:46:45 +0100

    Break too complex methods

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ wamp_server
 ===========
 
 [![Build Status](https://secure.travis-ci.org/rubencaro/wamp_server.png?branch=master)](http://travis-ci.org/rubencaro/wamp_server)
+[![Gem Version](https://badge.fury.io/rb/wamp_server.png)](http://rubygems.org/gems/wamp_server)
+[![Code Climate](https://codeclimate.com/github/rubencaro/wamp_server.png)](https://codeclimate.com/github/rubencaro/wamp_server)
 
 [WAMPv1](http://wamp.ws/spec) compliant server to be used
 as a template for nice and shining Ruby apps, based on
--- lib/wamp_server.rb
+++ lib/wamp_server.rb
@@ -44,8 +44,7 @@ module WAMP
   def self.start_server(a_module, opts = {})
 
     # avoid more instances
-    a = a_module
-    a.extend a
+    a_module.extend a_module
 
     host = opts[:host] || '0.0.0.0'
     port = opts[:port] || '3000'
@@ -56,67 +55,85 @@ module WAMP
       trap("TERM") { WAMP.stop_server }
       trap("KILL") { WAMP.stop_server }
 
-      a.before_start if a.respond_to?(:before_start)
+      a_module.before_start if a_module.respond_to?(:before_start)
 
       puts "Listening on #{host}:#{port}"
       WebSocket::EventMachine::Server.start(:host => host, :port => port) do |ws|
 
         ws.onopen do
-          Fiber.new do
-            sid = a.onwelcome ws: ws
-            ws.send [WAMP::WELCOME, sid, 1, WAMP.stamp].to_json
-          end.resume
+          onopen ws, a_module
         end
 
         ws.onmessage do |msg, type|
-          Fiber.new do
-            begin
-              call = JSON.parse msg
-            rescue JSON::ParserError
-              ws.send( WAMP::RESPONSES[:not_json] )
-            end
-
-            if call.first == WAMP::CALL then
-              # [ TYPE_ID_CALL , callID , procURI , ... ]
-              _, callid, curie, *args = call
-              begin
-                result = a.oncall ws: ws, curie: curie, args: args
-                ws.send [WAMP::CALLRESULT, callid, result].to_json
-              rescue => ex
-                ws.send [WAMP::CALLERROR, callid, "http://error", ex.to_s].to_json
-              end
-            elsif call.first == WAMP::PUBLISH then
-              # [ TYPE_ID_PUBLISH , topicURI , event , exclude , eligible ]
-              _, curie, event, excluded, eligible = call
-              result = a.onpublish curie: curie
-              dest = (result[:subscribed] & eligible) - excluded
-              package = [WAMP::EVENT, result[:uri], event].to_json
-              dest.each do |d|
-                ObjectSpace._id2ref(d).send( package )
-              end
-            elsif call.first == WAMP::SUBSCRIBE then
-              # [ TYPE_ID_SUBSCRIBE , topicURI ]
-              a.onsubscribe ws: ws, curie: call[1]
-            elsif call.first == WAMP::UNSUBSCRIBE then
-              # [ TYPE_ID_UNSUBSCRIBE , topicURI ]
-              a.onunsubscribe ws: ws, curie: call[1]
-            elsif call.first == WAMP::PREFIX then
-              # [ TYPE_ID_PREFIX , prefix, URI ]
-              _, prefix, uri = call
-              a.onprefix ws: ws, uri: uri, prefix: prefix
-            end
-          end.resume
+          onmessage ws, msg, a_module
         end
 
         ws.onclose do
-          Fiber.new do
-            a.onclose ws: ws
-          end.resume
+          onclose ws, a_module
         end
 
       end # Server.start
     end # EM.run
 
+  end
+
+  def self.onopen(ws, a_module)
+    Fiber.new do
+      sid = a_module.onwelcome ws: ws
+      ws.send [WAMP::WELCOME, sid, 1, WAMP.stamp].to_json
+    end.resume
+  end
+
+  def self.onclose(ws, a_module)
+    Fiber.new do
+      a_module.onclose ws: ws
+    end.resume
+  end
+
+  def self.onmessage(ws, msg, a_module)
+    Fiber.new do
+      begin
+        call = JSON.parse msg
+      rescue JSON::ParserError
+        ws.send( WAMP::RESPONSES[:not_json] )
+      end
+
+      if call.first == WAMP::CALL then
+        oncall ws, call, a_module
+      elsif call.first == WAMP::PUBLISH then
+        onpublish ws, call, a_module
+      elsif call.first == WAMP::SUBSCRIBE then
+        # [ TYPE_ID_SUBSCRIBE , topicURI ]
+        a_module.onsubscribe ws: ws, curie: call[1]
+      elsif call.first == WAMP::UNSUBSCRIBE then
+        # [ TYPE_ID_UNSUBSCRIBE , topicURI ]
+        a_module.onunsubscribe ws: ws, curie: call[1]
+      elsif call.first == WAMP::PREFIX then
+        # [ TYPE_ID_PREFIX , prefix, URI ]
+        _, prefix, uri = call
+        a_module.onprefix ws: ws, uri: uri, prefix: prefix
+      end
+    end.resume
+  end
+
+  def self.oncall(ws, call, a_module)
+    # [ TYPE_ID_CALL , callID , procURI , ... ]
+    _, callid, curie, *args = call
+    result = a_module.oncall ws: ws, curie: curie, args: args
+    ws.send [WAMP::CALLRESULT, callid, result].to_json
+  rescue => ex
+    ws.send [WAMP::CALLERROR, callid, "http://error", ex.to_s].to_json
+  end
+
+  def self.onpublish(ws, call, a_module)
+    # [ TYPE_ID_PUBLISH , topicURI , event , exclude , eligible ]
+    _, curie, event, excluded, eligible = call
+    result = a_module.onpublish curie: curie
+    dest = (result[:subscribed] & eligible) - excluded
+    package = [WAMP::EVENT, result[:uri], event].to_json
+    dest.each do |d|
+      ObjectSpace._id2ref(d).send( package )
+    end
   end
 
 end
